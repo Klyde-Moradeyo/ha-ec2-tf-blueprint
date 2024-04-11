1f91d44dbf4d6332dcc45594a52a3af7698b2f37
Klyde <klyde.moradeyo.dev@gmail.com>
2024-04-11 12:30:09 +0100

    Add outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "dns_name" {
+    description = "DNS name to reach the web server on"
+    value = module.load_balancer.dns_name
+}
+
+output "dns_name" {
+    description = "DNS name to reach the web server on"
+    value = module.load_balancer.dns_name
+}
